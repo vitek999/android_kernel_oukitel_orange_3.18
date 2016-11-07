9c25eb591970c1d8d6493d143b188be61dec1c30
vitek999 <vitek2012rus@gmail.com>
2016-11-07 12:53:14 +0000

    first commit

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export KBUILD_BUILD_USER=assusdan
+export KBUILD_BUILD_HOST=SRT
+
+if [ -f arch/arm/boot/zImage-dtb ]
+then
+    echo 'Remove kernel...'
+    echo ""
+    rm arch/arm/boot/zImage*
+fi
+
+echo "Git pull..."
+git pull >/dev/null
+echo ""
+
+echo "Export toolchains..."
+export ARCH=arm CROSS_COMPILE=../*5.2*/bin/arm-cortex-linux-gnueabi-
+echo ""
+
+echo "Make defconfig..."
+make benefit_m7_defconfig >/dev/null
+echo ""
+
+echo "Start build..."
+time make -j4 >/dev/null 2>errors.log
+echo ""
+
+if [ ! -f arch/arm/boot/zImage-dtb ]
+then
+    echo "BUILD ERRORS!"
+    echo "BUILD ERRORS!"
+    echo "BUILD ERRORS!"
+else
+    echo 'Moving...'
+    cp arch/arm/boot/zImage-dtb /var/www/html/boot.img-kernel
+    mv arch/arm/boot/zImage-dtb boot.img-kernel
+fi
+
+echo "======================"
+echo $[$SECONDS / 60]' minutes '$[$SECONDS % 60]' seconds' 
+echo "======================"
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 9 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#9 SMP PREEMPT Mon Nov 7 12:07:09 UTC 2016"
+#define LINUX_COMPILE_BY "vitek999"
+#define LINUX_COMPILE_HOST "SRT"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
