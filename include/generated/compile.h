7d2c5496a4a98031cd3165dd95b0885d31de52d7
vitek999 <vitek2012rus@gmail.com>
2016-11-08 16:15:39 +0200

    fix sd card

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Tue Nov 8 15:43:19 EET 2016"
+#define UTS_VERSION "#5 SMP PREEMPT Tue Nov 8 16:01:35 EET 2016"
 #define LINUX_COMPILE_BY "vitek999"
 #define LINUX_COMPILE_HOST "vitek999-SRT"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
