743a1bb3fc426b4a003b53a54f3dd734cb9dd356
vitek999 <vitek2012rus@gmail.com>
2016-11-08 15:16:08 +0200

    fix ncp1854

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 SMP PREEMPT Mon Nov 7 12:07:09 UTC 2016"
+#define UTS_VERSION "#2 SMP PREEMPT Tue Nov 8 15:08:40 EET 2016"
 #define LINUX_COMPILE_BY "vitek999"
-#define LINUX_COMPILE_HOST "SRT"
+#define LINUX_COMPILE_HOST "vitek999-SRT"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
