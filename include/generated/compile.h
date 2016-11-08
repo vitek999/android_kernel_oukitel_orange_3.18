dfc7c021dd56d62ff0be45fa0f52892e29223875
vitek999 <vitek2012rus@gmail.com>
2016-11-08 15:49:23 +0200

    fix bat tab 2

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Tue Nov 8 15:08:40 EET 2016"
+#define UTS_VERSION "#4 SMP PREEMPT Tue Nov 8 15:43:19 EET 2016"
 #define LINUX_COMPILE_BY "vitek999"
 #define LINUX_COMPILE_HOST "vitek999-SRT"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
