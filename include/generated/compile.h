55a70f36f7dbb3d2b22ca489d331f667963c668f
olegsvs <oleg.texet@gmail.com>
2017-06-15 07:44:25 +0000

    clean up

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Thu Jun 15 07:36:36 UTC 2017"
-#define LINUX_COMPILE_BY "smartrom"
-#define LINUX_COMPILE_HOST "SRT"
-#define LINUX_COMPILER "gcc version 4.8 (GCC) "
